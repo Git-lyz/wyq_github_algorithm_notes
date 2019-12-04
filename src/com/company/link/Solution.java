4a48ab33cb872c5f843e5e2fbe77a063f3adfe87
lyzqw <yuwq327037419@163.com>
2019-12-05 07:42:04 +0800

    feat: 合并有序的2个链表
    Signed-off-by: lyzqw <yuwq327037419@163.com>

---
+++ src/com/company/link/Solution.java
@@ -0,0 +1,81 @@
+package com.company.link;
+
+import com.company.sort.ReverseListNode;
+
+/**
+ * leetcode 21. 合并2个有序链表
+ *
+ * @author liuyuzhe
+ */
+public class Solution {
+    public static void main(String[] args) {
+        ReverseListNode link1 = buildLink1();
+        System.out.println();
+        System.out.println("==========");
+        ReverseListNode link2 = buildLink2();
+        ReverseListNode listNode = mergeTwoList2(link1, link2);
+        listNode.sequenceShow();
+    }
+
+    /**
+     * 递归: 判断l1 和 l2 哪一个元素更小, 然后递归找到尾节点, 再反向返回结果, 按顺序赋值对应的next节点
+     */
+    private static ReverseListNode mergeTwoList1(ReverseListNode l1, ReverseListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoList1(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoList1(l1, l2.next);
+            return l2;
+        }
+    }
+
+    /**
+     * 遍历: 使用一个哨兵preHead链表, 我们不断的调整他的next指针, 不断的比较2个链表的值, 把小的放入preHead链表中,
+     * 直到2个链表指向null了;
+     * 循环终止时, 2个链表最多有一个非空的, 放入合并的链表尾部
+     */
+    private static ReverseListNode mergeTwoList2(ReverseListNode l1, ReverseListNode l2) {
+        ReverseListNode preHead = new ReverseListNode(-1);
+        ReverseListNode prev = preHead;//维护一个临时指针
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        prev.next = l1 == null ? l2 : l1;
+        return preHead.next;//从-1指向下一个真正头节点
+    }
+
+
+    private static ReverseListNode buildLink1() {
+        ReverseListNode head = new ReverseListNode(1);
+        ReverseListNode node2 = new ReverseListNode(5);
+        head.after(node2);
+        ReverseListNode node3 = new ReverseListNode(8);
+        node2.after(node3);
+        return head;
+    }
+
+    private static ReverseListNode buildLink2() {
+        ReverseListNode head = new ReverseListNode(2);
+        ReverseListNode node2 = new ReverseListNode(6);
+        head.after(node2);
+        ReverseListNode node3 = new ReverseListNode(9);
+        node2.after(node3);
+        return head;
+    }
+
+
+}
--- src/com/company/sort/ReverseListNode.java
+++ src/com/company/sort/ReverseListNode.java
@@ -1,42 +1,43 @@
 package com.company.sort;
 
-class ReverseListNode {
-        ReverseListNode next;
-        ReverseListNode prev;
-        int data;
+public class ReverseListNode {
+    public ReverseListNode next;
+    public ReverseListNode prev;
+    public int val;
 
-        ReverseListNode(int data) {
-            this.data = data;
+    public ReverseListNode(int val) {
+        this.val = val;
+    }
+
+    public ReverseListNode after(ReverseListNode node) {
+        ReverseListNode nextNode = next;
+        this.next = node;
+        node.prev = this;
+        if (nextNode != null) {
+            node.next = nextNode;
+            nextNode.prev = node;
         }
+        return this;
+    }
 
-        ReverseListNode after(ReverseListNode node) {
-            ReverseListNode nextNode = next;
-            this.next = node;
-            node.prev = this;
-            if (nextNode != null) {
-                node.next = nextNode;
-                nextNode.prev = node;
-            }
-            return this;
-        }
+    public int getVal() {
+        return val;
+    }
 
-        public int getData() {
-            return data;
-        }
 
-        void sequenceShow() {
-            ReverseListNode next = this;
-            while (next != null) {
-                System.out.print(next.getData() + " ");
-                next = next.next;
-            }
-        }
-
-        void reverseShow(){
-            ReverseListNode curr = this;
-            while (curr != null) {
-                System.out.print(curr.getData() + " ");
-                curr = curr.prev;
-            }
+    public void sequenceShow() {
+        ReverseListNode next = this;
+        while (next != null) {
+            System.out.print(next.getVal() + " ");
+            next = next.next;
         }
     }
+
+    void reverseShow() {
+        ReverseListNode curr = this;
+        while (curr != null) {
+            System.out.print(curr.getVal() + " ");
+            curr = curr.prev;
+        }
+    }
+}
