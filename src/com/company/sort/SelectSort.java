cb50bf5fe81ad1272a3363836de84b2701f751a1
lyzqw <yuwq327037419@163.com>
2020-01-04 15:56:28 +0800

    feat: 加油站
    Signed-off-by: lyzqw <yuwq327037419@163.com>

--- src/com/company/sort/SelectSort.java
+++ src/com/company/sort/SelectSort.java
@@ -5,13 +5,14 @@ import java.util.Arrays;
 import static com.company.sort.HeapSort.swap;
 
 public class SelectSort {
+
     public static void main(String[] args) {
         int[] nums = {5, 6, 4, 7};
         System.out.println(Arrays.toString(selectSort(nums)));
     }
 
     private static int[] selectSort(int[] nums) {
-        for (int i = 0; i < nums.length-1; i++) {
+        for (int i = 0; i < nums.length - 1; i++) {
             int min = i;
             for (int j = i; j < nums.length; j++) {
                 if (nums[min] > nums[j]) {
