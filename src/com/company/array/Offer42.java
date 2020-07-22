ad0103bf668fe4bd5313b72b81355fef8e1e9cf2
lyzqw <yuwq327037419@163.com>
2020-07-23 05:55:12 +0800

    feat: 最大子串的值
    Signed-off-by: lyzqw <yuwq327037419@163.com>

---
+++ src/com/company/array/Offer42.java
@@ -0,0 +1,30 @@
+package com.company.array;
+
+/**
+ * 长度为n的字符串:
+ * 子串(必须连续挨着)有n(n+1)/2个
+ * <p>
+ * 子序列有2的n次方
+ * <p>
+ * 遇到不重复的首先想到Set
+ */
+public class Offer42 {
+
+    /**
+     * https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/
+     * -2,1,-3,4,-1,2,1,-5,4
+     */
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(arr));
+    }
+
+    private static int maxSubArray(int[] nums) {
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += Math.max(nums[i - 1], 0);
+            res = Math.max(nums[i], res);
+        }
+        return res;
+    }
+}
--- src/com/company/array/Offer53.java
+++ src/com/company/array/Offer53.java
@@ -4,15 +4,25 @@ package com.company.array;
  * 缺失的数字
  * https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof/
  */
-public class offer53 {
+public class Offer53 {
     public static void main(String[] args) {
 
         int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 9};
 
-        System.out.println(missingNumber(arr));
+        System.out.println(missNumber(arr));
     }
 
 
+    public static int missNumber(int[] nums){
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]!=i)return i;
+        }
+        return nums.length;
+    }
+
+    /**
+     * 有序的, 首先想使用二分法
+     */
     public static int missingNumber(int[] nums) {
         int low = 0, high = nums.length - 1;
 
