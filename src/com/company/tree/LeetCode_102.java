9fc8191e8f79b36ac3b7edb13255351ec25bc4da
lyzqw <yuwq327037419@163.com>
2020-07-30 06:45:42 +0800

    feat: 二叉树的层序遍历
    Signed-off-by: lyzqw <yuwq327037419@163.com>

---
+++ src/com/company/tree/LeetCode_102.java
@@ -0,0 +1,38 @@
+package com.company.tree;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 二叉树的层序遍历
+ * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/solution/di-gui-he-die-dai-by-powcai-2/
+ */
+public class LeetCode_102 {
+
+
+    public static void main(String[] args) {
+        TreeNode treeNode = TreeDataFactory.createTreeNode();
+        System.out.println(postOrderTraversal(treeNode));
+    }
+
+    private static List<List<Integer>> postOrderTraversal(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        helper(root, res, 0);
+        return res;
+    }
+
+    private static void helper(TreeNode root, List<List<Integer>> res, int depth) {
+        if (root == null) {
+            return;
+        }
+        if (depth == res.size()) {
+            res.add(new LinkedList<>());
+        }
+        res.get(depth).add(root.val);
+        helper(root.left, res, depth + 1);
+        helper(root.right, res, depth + 1);
+    }
+
+}
